fed4a9216161d46e3ba6bc488e8172908a2cd406
Arialdo Martini <arialdomartini@gmail.com>
2018-02-17 18:06:52 +0100

    A performance monitor that accepts only Func<T>

---
+++ FunctionalProgramming/Client.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using FluentAssertions;
+using Xunit;
+
+namespace FunctionalProgramming
+{
+    public class TestConsole : IDisposable
+    {
+        private readonly TextWriter _originalOut;
+
+        public TestConsole()
+        {
+            TextWriter = new StringWriter();
+            _originalOut = Console.Out;
+            Console.SetOut(TextWriter);
+        }
+
+        public readonly TextWriter TextWriter;
+        
+        public static implicit operator string(TestConsole testConsole)
+        {
+            return testConsole.TextWriter.ToString();
+        }
+
+        public void Dispose()
+        {
+            Console.SetOut(_originalOut);
+        }
+    }
+
+
+    public class Client
+    {
+        [Fact]
+        public void should_calculate_execution_time()
+        {
+            using (var t = new TestConsole())
+            {
+                PerformanceMonitor.Time("some op", () =>
+                {
+                    Thread.Sleep(300);
+                    return 100;
+                });
+
+                string result = t;
+                result.Should().Contain("some op took 300ms");
+            }
+        }
+    }
+}
---
+++ FunctionalProgramming/PerformanceMonitor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace FunctionalProgramming
+{
+    public class PerformanceMonitor : IDisposable
+    {
+        private readonly Stopwatch _stopWatch;
+
+        private PerformanceMonitor()
+        {
+            _stopWatch = new Stopwatch();
+            _stopWatch.Start();
+        }
+
+        public static T Time<T>(string op, Func<T> f)
+        {
+            using (var monitor = new PerformanceMonitor())
+            {
+                var result = f();
+
+                var swElapsedMilliseconds = monitor.ElapsedMilliseconds;
+            
+                Console.WriteLine($"{op} took {swElapsedMilliseconds}ms");
+                return result;                
+            }
+        }
+
+        private long ElapsedMilliseconds => _stopWatch.ElapsedMilliseconds;
+
+        public void Dispose()
+        {
+            _stopWatch.Stop();
+        }
+    }
+}
