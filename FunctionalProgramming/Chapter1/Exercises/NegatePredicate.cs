7f97767a073962433e6d10891e5e3c32e89d9c04
Arialdo Martini <arialdomartini@gmail.com>
2018-08-16 06:56:05 +0200

    Ch 1 - Exercise 2: negate a predicate

---
+++ FunctionalProgramming/Chapter1/Exercises/NegatePredicate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace FunctionalProgramming.Chapter1.Exercises
+{
+    public static class NegatePredicateExtension
+    {
+        public static Func<T, bool> Negate<T>(this Func<T, bool> predicate)
+            => i => !(predicate(i));
+    }
+    
+    public class NegatePredicate
+    {
+        [Fact]
+        public void should_negate_a_unary_predicate()
+        {
+            Func<int, bool> predicate = i => i % 2 == 0;
+
+            var listTrue = new List<int> {2, 4, 6, 8, 10};
+            var listFalse = new List<int> {1, 3, 5, 7, 9};
+
+            listTrue.All(i => predicate(i) == true);
+            listFalse.All(i => predicate(i) == false);
+
+            var negate = predicate.Negate();
+                        
+            listTrue.All(i => negate(i) == false);
+            listFalse.All(i => negate(i) == true);
+
+        }
+    }
+}
