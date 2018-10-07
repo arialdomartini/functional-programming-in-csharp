9ee0981e4e17d3debdd91308d7f60ed4c5736d07
Arialdo Martini <arialdomartini@gmail.com>
2018-10-07 11:31:28 +0200

    Dealing with optional name by using Match

---
+++ FunctionalProgramming/Chapter3/OptionalNameExampleTest.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace FunctionalProgramming.Chapter3
+{
+    public struct Subscriber
+    {
+        public Option<string> Name { get; set; }
+        public string Email { get; set; }
+    }
+
+    public static class Subscription
+    {
+        public static string GreetingsFor(Subscriber subscriber) =>
+            $"Dear {subscriber.Name.Match(() => "subscriber", r => r)}, your mail is {subscriber.Email}";
+    }
+    public class OptionalNameExampleTest
+    {
+        [Fact]
+        public void should_greet_subscribers_who_provided_their_name()
+        {
+            var subscriber = new Subscriber {Name = "Mario", Email = "tux@tux.com"};
+
+            var result = Subscription.GreetingsFor(subscriber);
+
+            result.Should().Be("Dear Mario, your mail is tux@tux.com");
+        }
+        
+        [Fact]
+        public void should_greet_subscribers_who_didn_t_provide_names()
+        {
+            var subscriber = new Subscriber {Email = "tux@tux.com"};
+
+            var result = Subscription.GreetingsFor(subscriber);
+
+            result.Should().Be("Dear subscriber, your mail is tux@tux.com");
+        }
+    }
+}
